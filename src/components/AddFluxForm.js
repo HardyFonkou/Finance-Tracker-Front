2fc84a70d8c479930fc47312da428086ef090a39
HardyFonkou <nhfonkou@gmail.com>
2022-07-07 08:46:29 +0100

    Update Add Flux Form

--- src/components/AddFluxForm.js
+++ src/components/AddFluxForm.js
@@ -29,23 +29,23 @@ const AddFluxForm = () => {
     <>
         <h2 className='text-2xl uppercase font-bold text-blue-500'>Nouveau flux</h2>
         <form ref={formRef} onSubmit={(e) => handleSubmit(e)} className='flex flex-col space-y-4 pt-4 lg:flex-row lg:space-x-20 lg:space-y-0 lg:items-center'>
-          <label className='flex flex-col'><span className='font-semibold text-lg mr-4'>Date :</span>
-            <input ref={dateRef} className='border shadow-sm border-slate-300 focus:outline-none focus:border-blue-500 rounded-md mt-1 px-4 py-2 w-full lg:w-96' name='date' type='date' />
+          <label className='flex flex-col lg:w-1/5'><span className='font-semibold text-lg mr-4'>Date :</span>
+            <input ref={dateRef} className='border shadow-sm border-slate-300 focus:outline-none focus:border-blue-500 rounded-md mt-1 px-4 py-2 w-full' name='date' type='date' />
           </label>
 
-          <label className='flex flex-col'><span className='font-semibold text-lg mr-4'>Libellé :</span>
-            <input ref={labelRef} className='border shadow-sm border-slate-300 focus:outline-none focus:border-blue-500 rounded-md mt-1 px-4 py-2 w-full lg:w-96' name='label' type='text' />
+          <label className='flex flex-col lg:w-1/5'><span className='font-semibold text-lg mr-4'>Libellé :</span>
+            <input ref={labelRef} className='border shadow-sm border-slate-300 focus:outline-none focus:border-blue-500 rounded-md mt-1 px-4 py-2 w-full' name='label' type='text' />
           </label>
 
-          <label className='flex flex-col'><span className='font-semibold text-lg mr-4'>Type :</span>
-            <select ref={typeRef} className='border shadow-sm border-slate-300 focus:outline-none focus:border-blue-500 rounded-md mt-1 px-4 py-2 w-full lg:w-96'>
+          <label className='flex flex-col lg:w-1/5'><span className='font-semibold text-lg mr-4'>Type :</span>
+            <select ref={typeRef} className='border shadow-sm border-slate-300 focus:outline-none focus:border-blue-500 rounded-md mt-1 px-4 py-2 w-full'>
               <option value='in'>Entrée</option>
               <option value='out'>Sortie</option>
             </select>
           </label>
 
-          <label className='flex flex-col'><span className='font-semibold text-lg mr-4'>Montant :</span>
-            <input ref={amountRef} className='border shadow-sm border-slate-300 focus:outline-none focus:border-blue-500 rounded-md mt-1 px-4 py-2 w-full lg:w-96' name='amount' type='text' />
+          <label className='flex flex-col lg:w-1/5'><span className='font-semibold text-lg mr-4'>Montant :</span>
+            <input ref={amountRef} className='border shadow-sm border-slate-300 focus:outline-none focus:border-blue-500 rounded-md mt-1 px-4 py-2 w-full' name='amount' type='text' />
           </label>
 
           <button type='submit' className='mx-auto lg:mx- lg:self-end bg-blue-500 text-white text-lg font-semibold max-w-fit px-4 py-2 hover:bg-blue-600'>Enregistrer</button>
